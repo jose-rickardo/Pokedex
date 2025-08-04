4e22a9ebdbf9e792689936c72165b944be5c28f6
Warry <hei.warry@gmail.com>
2025-08-04 18:19:36 +0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import { PokedexPage } from "./pages/PokedexPage";
+import SearchBar from "./components/SearchBar";
+import TypeFilter from "./components/TypeFilter";
+import PokemonGrid from "./components/PokemonGrid";
+import useAppState from "./hooks/useAppState";
 
-function App() {
-  return <PokedexPage/>;
+export default function App() {
+  const {
+    search,
+    setSearch,
+    selectedType,
+    setSelectedType,
+    filteredPokemons,
+    loading,
+    selectedPokemon,
+    setSelectedPokemon,
+  } = useAppState();
+
+  return (
+    <div>
+      <div className="flex dark:bg-gray-900 justify-center">
+        <SearchBar search={search} setSearch={setSearch} />
+        <TypeFilter selectedType={selectedType} setSelectedType={setSelectedType} />
+      </div>
+      <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-800 dark:text-white transition">
+        <PokemonGrid
+          pokemons={filteredPokemons}
+          loading={loading}
+          selectedPokemon={selectedPokemon}
+          setSelectedPokemon={setSelectedPokemon}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default App;
